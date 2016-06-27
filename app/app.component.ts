bc1ceffe197cfcfdc9c531abf926864da66bdc06
loicao <lcao1802@gmail.com>
2016-06-28 01:49:49 +0700

    step 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,10 @@
 import {Component} from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 @Component({
     selector: 'my-app',
     template: `
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
         <h2>My Heroes</h2>
         <ul class="heroes">
             <li *ngFor="let hero of heroes" (click)="onSelect(hero)"
@@ -72,7 +61,8 @@ export class Hero {
                 margin-right: .8em;
                 border-radius: 4px 0 0 4px;
             }
-            `]
+            `],
+        directives: [HeroDetailComponent]
 })
 export class AppComponent { 
     title = 'Tour of Heroes';
