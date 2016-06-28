57f5d706570ffbe6a68900feaea04cef7e427869
loicao <lcao1802@gmail.com>
2016-06-28 23:34:11 +0700

    step 5

---
+++ app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return new Promise<Hero[]>(resolve =>
+        setTimeout(() => resolve(HEROES), 5000));
+    }
+}
