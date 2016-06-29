fdb59bec6bccd5c41329a621c5cc3461fed3d702
loicao <lcao1802@gmail.com>
2016-06-29 22:48:14 +0700

    step 6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,43 +1,24 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
 import { HeroService } from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { DashboardComponent } from './dashboard.component';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
-    selector: 'my-app',
-    template : `
-        <h1>{{title}}</h1>
-        <nav>
-            <a [routerLink]="['Dashboard']">Dashboard</a>
-            <a [routerLink]="['Heroes']">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `,
-    styleUrls: ['app/app.component.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [ROUTER_PROVIDERS, HeroService] 
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+      <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HeroService
+  ]
 })
-@RouteConfig([
-  {
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  },
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  }
-])
 export class AppComponent {
-    title: 'Tour of Heroes';
-
+  title = 'Tour of Heroes';
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,33 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
-import { Router } from '@angular/router-deprecated';
+
 @Component({
-    selector: 'my-heroes',
-    templateUrl : 'app/heroes.component.html',
-    styleUrls: ['app/heroes.component.css'],
-    directives: [HeroDetailComponent]
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls:  ['app/heroes.component.css']
 })
-export class HeroesComponent implements OnInit { 
-    title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
 
-    constructor(private heroService: HeroService, private router: Router) {}
-    onSelect(hero: Hero) { this.selectedHero = hero; }
-    getHeroes() {
-        this.heroService.getHeroes()
-            .then(heroes => 
-                this.heroes = heroes);
-    }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
-    ngOnInit() {
-        this.getHeroes();
-    }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 
-    gotoDetail() {
-        this.router.navigate(['HeroDetail', {id: this.selectedHero.id}]);
-    }
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  gotoDetail() {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
-
